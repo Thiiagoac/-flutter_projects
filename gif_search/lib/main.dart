113139475bd1372272252e85ca4164636eb0ef96
Thiago Araujo <thiiagoac@hotmail.com>
2024-10-05 13:25:28 -0300

    finish gif search

--- gif_search/lib/main.dart
+++ gif_search/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:gif_search/ui/gif_page.dart';
 import 'package:gif_search/ui/home_page.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: HomePage(),
-      theme: ThemeData(hintColor: Colors.white),
+      theme: ThemeData(hintColor: Colors.white, appBarTheme: AppBarTheme(color: Colors.white)),
     )
   );
 }
